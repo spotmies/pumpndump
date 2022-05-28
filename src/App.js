200be66697f8ef957974fc5e930ee3a14c1a469d
sekharjavvadi <sekharatece@gmail.com>
2022-05-28 16:36:56 +0530

    metamask integrated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import { useState } from "react";
+import { ethers } from "ethers";
 function App() {
+  const [wallets, setWallets] = useState();
+
+  async function requestAccount() {
+    console.log("Button working");
+
+    if (window.ethereum) {
+      console.log("Metamask Detected");
+
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setWallets(accounts[0]);
+      } catch (error) {
+        console.log("Error connecting....");
+      }
+    } else {
+      console.log("Metamask not detected");
+    }
+  }
+
+  async function connectWallet() {
+    if (typeof window.ethereum !== "undefined") {
+      await requestAccount();
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,7 +38,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <button class="button button4">Button</button>
+        <button class="button button4" onClick={requestAccount}>
+          Button
+        </button>
       </header>
     </div>
   );
