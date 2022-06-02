d1326693cc53fdefa1f0946df218ebb2fef22677
sekharjavvadi <sekharatece@gmail.com>
2022-06-02 17:30:17 +0530

    constanst files implemnted

---
+++ src/pages/constants.js
@@ -0,0 +1,58 @@
+module.exports = Object.freeze({
+  roadMapList: [
+    {
+      q: "Day 1",
+      ans: "Everyone should only list at 0.03 or higher in secondary market after mint to bring some traction..",
+    },
+    {
+      q: "Day 2",
+      ans: "Everyone should only list at 0.09 or higher in secondary market to get that sweet sweet volume…",
+    },
+    {
+      q: "Day 3",
+      ans: "Stop undercuttin Eachother A*SH*LEs, we have plan.. List at 0.2 or higher",
+    },
+    {
+      q: "Day 4",
+      ans: "Alright it feels good to stay in top 20 for 4 days Straight..list at 0.5 now!!",
+    },
+    {
+      q: "Day 5",
+      ans: "Don't Panick… we're almost There…List at 1 eth or higher!!",
+    },
+    {
+      q: "Day 6",
+      ans: "If everything is going according to plan we'll be at 1.5 Eth by now… if not, list it at 1.5 eth or more",
+    },
+    {
+      q: "Day 7",
+      ans: "@ 23:59",
+    },
+  ],
+  faq: [
+    {
+      q: "1. How do I mint?",
+      ans: "You can connect your wallet on PC using the Metamask extension for the Chrome browser. If you are on mobile you can connect through the Metamask in-app browser or connect through the DApp browser in the Trustwallet app. You can connect your wallet with Walletconnect.",
+    },
+    {
+      q: "2. How to buy/sell on secondary Market?",
+      ans: "You can buy/sell items in our Collection on secondary market by clicking on our official OS link on top right corner..",
+    },
+    {
+      q: "3. How many NFTs I can Mint?",
+      ans: "3 Per each Wallet to give more people a chance to get in the project.",
+    },
+    {
+      q: "4.How to get Rich?",
+      ans: "Read Questions 1.and 2.",
+    },
+    {
+      q: "5. Wen moon?",
+      ans: "Day 7.",
+    },
+    {
+      q: "*Not Financial Advice!",
+      ans: "Terms and conditions are don't sue when you lose.! Click Agree to mint lol",
+    },
+  ],
+});
